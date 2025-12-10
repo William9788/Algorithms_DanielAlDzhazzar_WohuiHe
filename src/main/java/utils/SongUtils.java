c072ce80f5a4d37d2a9894470d8b2c34bf853f2a
William9788 <hewohui9788@icloud.com>
2025-12-10 02:08:01 +0000

    feat:AddlastIndexOf() function code ;to fit in findMostCommonTag()method;Add findMostCommonTag()method code

--- src/main/java/utils/SongUtils.java
+++ src/main/java/utils/SongUtils.java
@@ -12,6 +12,7 @@ public class SongUtils {
      * @param songs the array of the songs to serach.
      * @param tag the tag to serach.
      * @return the array contains the specific tags
+     * @author Wohui He
      */
     public static Song[] searchByTag(Song [] songs, String tag){
         if(tag==null||tag.trim().isEmpty()){
@@ -39,7 +40,8 @@ public class SongUtils {
         // Should implement linear search, but not for a single result
     }
 
-  /*   public static Song searchBySongTitle(Song [] songs, String songTitle){
+  /* @author Wohui He   
+  public static Song searchBySongTitle(Song [] songs, String songTitle){
        if(songTitle==null||songTitle.trim().isEmpty()){
             return null;
         }
@@ -79,6 +81,7 @@ public class SongUtils {
 /**
  *  sort the provided array of songs in descending order based on the quantity of tags.
  * @param songs the array of songs going to be sorted by count tag.
+ * @author Wohui He
  */
     public static void sortSongsByNumTags(Song [] songs){
         for(int i=0;i<songs.length-1;i++){
@@ -99,7 +102,55 @@ public class SongUtils {
     }
 
     public static String findMostCommonTag(Song [] songs){
+        int totalTags=0;
+        for(int i =0;i<songs.length;i++){
+            if(songs[i]!=null){
+                totalTags+=songs[i].getTagCount();
+            }
+        }
+        if(totalTags==0){
+            return null;
+        }
+        String[] allTags=new String [totalTags];
+        int index=0;
+        for(int i=0;i<songs.length;i++){
+            if(songs[i]!=null){
+                String[] tags=songs[i].getTags();
+                int count = songs[i].getTagCount();
+                for(int x=0;x<count;x++){
+                    allTags[index]=tags[x].toLowerCase();
+                    index++;
+                }
+            }
+        }
+        int maxCount=0;
+        String MostCommonTag=null;
+        for(int i=0;i<songs.length;i++){
+            String current=allTags[i];
+            int frequency=1;
+            for(int x=i+1;x<allTags.length;x++){
+                if(allTags[x].equals(current)){
+                    frequency++;
+                }
+            }
+            if(i>lastIndexOf(MostCommonTag,allTags)&&frequency>maxCount||frequency==maxCount){
+                maxCount=frequency;
+                MostCommonTag=current;
+            }
+        }
+        return MostCommonTag;
         // todo: ADD findMostCommonTag() LOGIC
-        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    private static int lastIndexOf(String tag,String[]allTags){
+        if(tag==null){
+            return-1;
+        }
+        int position=-1;
+        for(int i=0;i<allTags.length;i++){
+            if(allTags[i].equals(tag)){
+                position=i;
+            }
+        }
+        return position;
     }
 }
